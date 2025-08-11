d0dd258b7e683be23e87acc75fae654f02eabd75
7t33n <7t33n@proton.me>
2025-08-11 15:39:07 +0200

    Quartz sync: Aug 11, 2025, 3:39 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
+    pageTitle: "7t33n.xyz",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
